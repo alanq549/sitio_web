8f9e4102e3686e7a48a345e38ca18a62a6c967e5
Alan Quintana <alanqff@gmail.com>
2024-11-24 14:25:55 -0600

    1.2.2
    otra prueba

--- js/sidebar.js
+++ js/sidebar.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Si está visible, lo cerramos
             sidebar.classList.remove('visible');
             content.classList.remove('sidebar-visible');
-            botones.classList.remove('sidebar-visible'); // Mover los botones de vuelta
+            botones.classList.remove('sidebar-visible');
             toggleButton.textContent = '☰'; // Cambiar a icono de abrir
 
             // Destruir el tooltip actual y crear uno nuevo
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Si está cerrado, lo abrimos
             sidebar.classList.add('visible');
             content.classList.add('sidebar-visible');
-            botones.classList.add('sidebar-visible'); // Mover los botones junto con el contenido
+            botones.classList.add('sidebar-visible');
             toggleButton.textContent = '☰'; // Cambiar a icono de cerrar
 
             // Destruir el tooltip actual y crear uno nuevo
